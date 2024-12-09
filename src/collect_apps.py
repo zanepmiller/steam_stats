6b2d831517924c190541fcfdec2118b2b010aff7
zmiller <zane@zanepmiller.com>
2024-12-09 15:58:39 -0700

    Application collection setup.

--- src/collect_apps.py
+++ src/collect_apps.py
@@ -1,4 +1,17 @@
+import os
+import sys
+import time
 
+import requests as req
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import lxml #   bs4 documentation says this is faster than the standard Python html parser
 
 if __name__ == '__main__':
-    pass
+#region Argument Parsing
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+#endregion
