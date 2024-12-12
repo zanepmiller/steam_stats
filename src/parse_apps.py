25fbcfb0404853c457e43a7058b4b9f9b19aec9c
zmiller <zane@zanepmiller.com>
2024-12-12 16:04:32 -0700

    Known Unicode encoding error for Chinese characters in applist.

--- src/parse_apps.py
+++ src/parse_apps.py
@@ -2,9 +2,37 @@ import json
 import os
 import datetime
 
+import numpy as np
+import pandas as pd
+
 import requests as req
 from bs4 import BeautifulSoup
 import lxml #   bs4 documentation says this is faster than the standard Python html parser
 
+def read_applist(target : str) -> pd.DataFrame:
+    if not os.path.isfile(target) : return None
+    with open(target, encoding='utf-8') as j_file:
+        try:
+            app_j = json.load(j_file)
+            return pd.read_json(app_j)
+        except json.JSONDecodeError:
+            return None
+
 if __name__ == '__main__':
-    pass
+#region Argument Parsing
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', required=True)
+
+    args = parser.parse_args()
+#endregion
+
+#region App Request
+    applist_path = os.path.abspath(args.target)
+    if not os.path.isfile(applist_path):
+        print(f"Provided target {args.target} resolved to {applist_path}, which is not a valid file.")
+        exit(0)
+
+    app_df = read_applist(os.path(applist_path))
+
+#endregion
--- test/parse_apps_test.py
+++ test/parse_apps_test.py
@@ -1,2 +1,68 @@
 import pytest
 
+import os
+
+from src import parse_apps as pa
+
+#region Test Values
+INV_PATH = f"Z:/invalid/path"
+J_TXT = '''
+{
+ "applist": {
+  "apps": [
+   {
+    "appid": 1941401,
+    "name": "Test App 0"
+   },
+   {
+    "appid": 2170321,
+    "name": "Test App 1: 骸骷王"
+   },
+   ]
+}
+'''
+
+@pytest.fixture(scope='session')
+def text_file(tmp_path_factory):
+    txt = f"Invalid text"
+    fn = tmp_path_factory.mktemp('test') / 'test.txt' 
+    with open(fn, 'w') as o_file:
+        print(txt, file=o_file)
+    return fn
+
+@pytest.fixture(scope='session')
+def poison_json_file(tmp_path_factory):
+    txt = f"Invalid text"
+    fn = tmp_path_factory.mktemp('test') / 'test.json'
+    with open(fn, 'w') as o_file:
+        print(txt, file=o_file)
+    return fn
+
+@pytest.fixture(scope='session')
+def json_file(tmp_path_factory):
+    txt = J_TXT
+    fn = tmp_path_factory.mktemp('test') / 'test.json'
+    with open(fn, 'w') as o_file:
+        print(txt, file=o_file)
+    return fn
+
+#endregion
+
+#region read_applist
+def test_read_applist_no_file():
+    ### Tests an invalid filename returns None
+    assert pa.read_applist(INV_PATH) == None
+
+def test_read_applist_not_json(text_file):
+    ### Tests a non-json file returns None
+    assert pa.read_applist(text_file) == None
+
+def test_read_applist_bad_json(poison_json_file):
+    ### Tests an invalid json file returns None
+    assert pa.read_applist(poison_json_file) == None
+
+def test_read_applist_test_json(json_file):
+    ### Tests an invalid json file returns None
+    assert pa.read_applist(json_file) == None
+
+#endregion
